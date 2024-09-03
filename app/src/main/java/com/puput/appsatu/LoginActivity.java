8c17d8bb9fc15f36d06355df1503dbfc1496b69f
puth-2 <puthirma1008@gmail.com>
2024-09-03 21:51:22 +0700

    Login

--- app/src/main/java/com/puput/appsatu/LoginActivity.java
+++ app/src/main/java/com/puput/appsatu/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.puput.appsatu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private String username;
-    private String password;
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,32 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+       etUsername = findViewById(R.id.etUsername);
+       etPassword = findViewById(R.id.etPassword);
+       btnLogin = findViewById(R.id.btnLogin);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (etUsername.getText().toString().equalsIgnoreCase(username) && etPassword.getText().toString()
+                        .equalsIgnoreCase(password)) {
+                    Intent Login = new Intent(LoginActivity.this, DashboardActivity.class);
+                    startActivity(Login);
+
+                    Toast.makeText(LoginActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Username atau Password salah", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
